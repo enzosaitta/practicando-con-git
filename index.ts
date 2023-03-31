c22c2088faefa8bd5d92c0068093bd8094d52bd5
Enzo Saitta <77999947+enzosaitta@users.noreply.github.com>
2023-03-31 00:05:58 -0300

    commit desde git hub

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import { getAll, getByAge } from "./employees";
 function main() {
   console.table(getAll());
   console.table(getByAge(26));
+  console.log("Hola enzo, aca se hizo un cambio piola");
+  
+  
 }
 
 main();
